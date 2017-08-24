74f3bb7bc349afc2f0b245228a1cfda9bd9d0a30
Tyler Walker <twalker@erskine247.com>
2017-08-24 18:35:53 -0400

    Added a "running" message

--- bot.py
+++ bot.py
@@ -8,6 +8,8 @@ api = twitter.Api(consumer_key = consumerKey, consumer_secret = consumerSecret, 
 lastImageUrl = ""
 
 def checkReddit():
+	if lastImageUrl == "":
+		api.PostUpdate("I'm running!")
 	with requests.get('https://www.reddit.com/r/megumin/new.json', headers={'user-agent': 'OreganoMeguminBot'}) as url:
 		meguminData = json.loads(url.content)['data']['children'][0]['data']
 		meguminImage = meguminData['url']
