e402efb0d6999bb097034cf9a513f7289db1d8fc
Tyler Walker <twalker@erskine247.com>
2017-08-29 01:32:42 -0400

    Moved to a dropbox solution

--- bot.py
+++ bot.py
@@ -1,25 +1,18 @@
-import twitter, json, requests, time, os
+import twitter, json, requests, time, os, dropbox, random, math
 
 consumerKey = os.environ['consumer_key']
 consumerSecret = os.environ['consumer_secret']
 accessTokenKey = os.environ['access_token_key']
 accessTokenSecret = os.environ['access_token_secret']
 api = twitter.Api(consumer_key = consumerKey, consumer_secret = consumerSecret, access_token_key = accessTokenKey, access_token_secret = accessTokenSecret)
-lastImageUrl = ""
+client = dropbox.Dropbox(os.environ['megumin_dropbox_key'])
 
-def checkReddit():
-	global lastImageUrl
-	if lastImageUrl == "":
-		api.PostUpdate("I'm running!")
-	with requests.get('https://www.reddit.com/r/megumin/new.json', headers={'user-agent': 'OreganoMeguminBot'}) as url:
-		meguminData = json.loads(url.content)['data']['children'][0]['data']
-		meguminImage = meguminData['url']
-		meguminLink = "https://reddit.com" + meguminData['permalink']
-		if (meguminImage != lastImageUrl):
-			api.PostMedia(meguminLink, meguminImage)
-			lastImageUrl = meguminImage
-			#checkReddit()
+def postMegumin():
+	meguminFolder = client.files_list_folder('').entries
+	randomIndex = math.floor(random.random() * len(meguminFolder))
+	randomImage = client.sharing_create_shared_link('/' + meguminFolder[randomIndex].name).url[:-1] + '1'
+	api.PostMedia('', randomImage)
 
 while True: 
-	checkReddit()
+	postMegumin()
 	time.sleep(600) # sleep for 10 minutes
