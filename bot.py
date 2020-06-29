a8ccb3192e6bc4f7cf069c87e2b58c2cc4dfbdc2
emrazyx <58108732+emrazyx@users.noreply.github.com>
2020-06-30 00:12:39 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,7 @@
 import twitter, json, requests, time, os, dropbox, random, math
 
-consumerKey = os.environ['consumer_key']
-consumerSecret = os.environ['consumer_secret']
-accessTokenKey = os.environ['access_token_key']
-accessTokenSecret = os.environ['access_token_secret']
-api = twitter.Api(consumer_key = consumerKey, consumer_secret = consumerSecret, access_token_key = accessTokenKey, access_token_secret = accessTokenSecret)
+
+api = twitter.Api(consumer_key = '4tycy2cbPBmuiot87SoUPSQZF', consumer_secret = '8U0ltWsK14F7MY1T7TvSy88h856cBP8VOHLkIgCIlyifzIaftP', access_token_key = '1120022500862234626-A488oXn3YFqkmZKQ6bMeeLL2aHDWZr', access_token_secret = 'SXSqg1IvXjRGM3xqVKcKNMLZFRLVXnfbgIVTPDTbFxyE8')
 client = dropbox.Dropbox(os.environ['megumin_dropbox_key'])
 
 def postMegumin():
